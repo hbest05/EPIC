c8d766c5584d87f92f407ee3d2c90090f112bbc6
holly jane 3 <hollyjanebest@gmail.com>
2026-05-29 10:51:11 +0100

    Delete client-cpp/build-47292 directory

--- client-cpp/build-47292/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SecureMsg_FILE C:/Users/m4tth/EPIC/client-cpp/build-47292/SecureMsg.exe)
-set(__QT_DEPLOY_TARGET_SecureMsg_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SecureMsg_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6WebSockets.dll;C:/vcpkg/installed/x64-windows/bin/libcurl.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
